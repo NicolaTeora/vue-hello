8a259c9e9f173e2ccec187c6ae5a267a6d95b041
NicolaTeora <nikxodia@gmail.com>
2024-01-30 09:50:42 +0100

    feat : add img

--- JS/main.js
+++ JS/main.js
@@ -5,9 +5,10 @@ createApp({
       return {
         message: 'Hello Vue!',
         secondMessage: "look me! I'm using Vue",
+        logo: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Total_War_logo.png',
 
         textMessage: {
-            titleMessage: 'Title message',
+            titleMessage: 'My Favourite game',
             textAreaMes: ''
         }
       }
